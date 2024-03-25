a48aa4b28b214976884b409e223b39ab976dc0e8
Alan Perez <alansilvaprz@gmail.com>
2024-03-25 01:13:16 -0300

    login e registro tentativa

---
+++ src/app/services/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './auth.service';
+
+@NgModule({
+  imports: [
+    HttpClientModule
+  ],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>('url_do_seu_backend/login', { email, password });
+  }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post<any>('url_do_seu_backend/register', { email, password });
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('isLoggedIn');
+  }
 }
